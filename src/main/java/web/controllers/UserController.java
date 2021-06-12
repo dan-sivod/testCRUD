b550fa6e10b9bb84bc022e4ec91e4124d3def7a3
89104775491Ds <dan-sivod@mail.ru>
2021-06-12 16:24:44 +0300

    Реализовано добавление пользователя

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class UserController {
 
@@ -22,35 +19,19 @@ public class UserController {
 
     @GetMapping(value ="/")
     public String printUsers(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("I'm Spring MVC application");
-        messages.add("5.2.0 version by sep'19 ");
-        model.addAttribute("messages", messages);
-
-        User user = new User("1","1",1);
-        userService.createUser(user);
-        System.out.println(userService.readUser(1));
-        return "index";
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
     }
 
-//    @GetMapping(values ="/{id}")
-//    public String showUser(@PathVariable("id") long id, ModelMap model) {
-//        model.addAttribute("id", userService.readUser(id));
-//        return "user1";
-//    }
+    @PostMapping()
+    public String createUser(@ModelAttribute("User") User user) {
+        userService.createUser(user);
+        return "redirect:/";
+    }
 
-//    @PostMapping(values ="/new")
-//    public String showUser(@RequestParam("name") String name, @RequestParam("lastName") String lastName,
-//                           @RequestParam("age") int age, ModelMap model) {
-//        User user = new User(name,lastName,age);
-//        model.addAttribute("user", user);
-//        return "new";
-//    }
-
-//    @GetMapping("/new")
-//    public String newUser(ModelMap model) {
-//        model.addAttribute("user", new User());
-//        return "new";
-//    }
+    @GetMapping("/new")
+    public String newUser(ModelMap model) {
+        model.addAttribute("user", new User());
+        return "new";
+    }
 }
