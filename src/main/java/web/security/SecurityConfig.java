55bc0cf660bdd0feeee5df419670993b1e177602
Danila Sivodedov <dan-sivod@mail.ru>
2021-06-28 13:21:36 +0300

    Добавлена конфигурация формы,страницы регистрации и выхода

--- src/main/java/web/security/SecurityConfig.java
+++ src/main/java/web/security/SecurityConfig.java
@@ -6,46 +6,73 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import web.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
     private final UserService userService;
 
-    @Autowired
     private final LoginSuccessHandler loginSuccessHandler;
 
-
-    public SecurityConfig(@Qualifier("userServiceImp") UserService userService, LoginSuccessHandler loginSuccessHandler) {
+    @Autowired
+    public SecurityConfig(@Qualifier("userServiceImp") UserService userService,
+                          @Qualifier("loginSuccessHandler") LoginSuccessHandler loginSuccessHandler) {
         this.userService = userService;
         this.loginSuccessHandler = loginSuccessHandler;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(11);
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userService);///!!!!!!!!!!!!!!!!!!!!!!!!!
+        authenticationProvider.setUserDetailsService(userService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .formLogin()
+                // указываем страницу с формой логина
+                .loginPage("/login")
+                // указываем action с формы логина
+                .loginProcessingUrl("/login")
+                .usernameParameter("j_username")
+                .passwordParameter("j_password")
+                .successHandler(loginSuccessHandler) // подключаем наш SuccessHandler для перенеправления по ролям
+                .permitAll();
+
+        http.logout()
+                .permitAll()
+                // указываем URL логаута
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                // указываем URL при удачном логауте
+                .logoutSuccessUrl("/login?logout");
+
+        http.authorizeRequests()// делаем страницу регистрации недоступной для авторизированных пользователей
+                //страницы аутентификаци доступна всем
+                .antMatchers("/login").anonymous()
+                .antMatchers("/user").access("hasAnyRole('ADMIN', 'USER')")
+                .antMatchers("/admin/**").access("hasAnyRole('ADMIN')")
+                // защищенные URL
+                .antMatchers("/hello").access("hasAnyRole('ADMIN')").anyRequest().authenticated();
+    }
 }
